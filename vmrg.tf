9d119523e02768ef6e36a3e3e5ed9c52c711c8ba
wkaleem14 <w.kaleem14@gmail.com>
2020-08-24 01:32:56 -0400

    Add files via upload

---
+++ vmrg.tf
@@ -0,0 +1,14 @@
+provider "azurerm" {
+    # The "feature" block is required for AzureRM provider 2.x.
+    # If you're using version 1.x, the "features" block is not allowed.
+    version = "~>2.0"
+    features {}
+}
+resource "azurerm_resource_group" "myterraformgroup" {
+    name     = "TerraformVM"
+    location = "eastus"
+
+    tags = {
+        environment = "Terraform Demo"
+    }
+}
---
+++ vmvnet.tf
@@ -0,0 +1,10 @@
+resource "azurerm_virtual_network" "myterraformnetwork" {
+    name                = "myVnet"
+    address_space       = ["10.0.0.0/16"]
+    location            = "eastus"
+    resource_group_name = azurerm_resource_group.myterraformgroup.name
+
+    tags = {
+        environment = "Terraform Demo"
+    }
+}
